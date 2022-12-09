8d0030ab58e73e65c97977ba6a433de44fdb6180
Jade Ferguson <jadesnider2440@gmail.com>
2022-12-09 01:07:44 -0800

    Added to notes tab

--- app/src/main/java/com/example/cosplanner/MainActivity.kt
+++ app/src/main/java/com/example/cosplanner/MainActivity.kt
@@ -41,3 +41,4 @@ class MainActivity : AppCompatActivity() {
                 || super.onOptionsItemSelected(item)
     }
 }
+
--- app/src/main/java/com/example/cosplanner/NotesFragment.kt
+++ app/src/main/java/com/example/cosplanner/NotesFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.cosplanner.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment() {
     override fun onCreateView(
@@ -12,4 +16,31 @@ class NotesFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_notes, container, false)
     }
+
+    private lateinit var binding: FragmentNotesBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = FragmentNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            val supportFragmentManager = null
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.name.observe(this){
+            binding.taskName.text = String.format("Task Name: %s", it)
+        }
+        taskViewModel.desc.observe(this){
+            binding.taskDesc.text = String.format("Task Desc: %s", it)
+        }
+    }
+
+    private fun setContentView(root: RelativeLayout) {
+
+    }
 }
+

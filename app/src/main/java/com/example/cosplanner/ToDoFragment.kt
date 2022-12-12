8fb3314a6bd8b6f2ab06f9f9b72123333a92da98
Jade Ferguson <jadesnider2440@gmail.com>
2022-12-12 15:21:52 -0800

    ToDo works and has working database

--- app/src/main/java/com/example/cosplanner/ToDoFragment.kt
+++ app/src/main/java/com/example/cosplanner/ToDoFragment.kt
@@ -1,13 +1,13 @@
 package com.example.cosplanner
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.cosplanner.databinding.FragmentToDoBinding
 
 class ToDoFragment : Fragment() {
@@ -30,13 +30,22 @@ class ToDoFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = TaskItemAdapter() { taskId ->
-            Toast.makeText(context, "Clicked task $taskId", Toast.LENGTH_SHORT).show()
+            viewModel.onTaskClicked(taskId)
         }
         binding.tasksList.adapter = adapter
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToTask.observe(viewLifecycleOwner, Observer { taskId ->
+            taskId?.let {
+                val action = ToDoFragmentDirections
+                    .actionToDoFragmentToEditTaskFragment(taskId)
+                this.findNavController().navigate(action)
+                viewModel.onTaskNavigated()
             }
         })
 
--- app/src/main/java/com/example/cosplanner/ToDoViewModel.kt
+++ app/src/main/java/com/example/cosplanner/ToDoViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.cosplanner
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -8,12 +10,23 @@ class ToDoViewModel(val dao: ToDoDao) : ViewModel() {
     var newTaskName = ""
     val tasks = dao.getAll()
 
+    private val _navigateToTask = MutableLiveData<Long?>()
+    val navigateToTask: LiveData<Long?>
+        get() = _navigateToTask
+
     fun addTask() {
         viewModelScope.launch {
             val task = ToDo()
             task.taskName = newTaskName
             dao.insert(task)
         }
+    }
+
+    fun onTaskClicked(taskId: Long) {
+        _navigateToTask.value = taskId
+    }
+    fun onTaskNavigated() {
+        _navigateToTask.value = null
     }
 
 }
